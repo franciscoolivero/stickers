de70f3e8b6e2616c7dc2ede2ba177150283d7d4d
Francisco Olivero <folivero@solstice.com>
2019-05-05 22:39:35 -0300

    Update Package name

--- Android/app/src/main/java/com/fracasitos/samplestickerapp/StickerPackLoader.java
+++ Android/app/src/main/java/com/fracasitos/samplestickerapp/StickerPackLoader.java
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.example.samplestickerapp;
+package com.fracasitos.samplestickerapp;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -22,18 +22,18 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
-import static com.example.samplestickerapp.StickerContentProvider.ANDROID_APP_DOWNLOAD_LINK_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.IOS_APP_DOWNLOAD_LINK_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.LICENSE_AGREENMENT_WEBSITE;
-import static com.example.samplestickerapp.StickerContentProvider.PRIVACY_POLICY_WEBSITE;
-import static com.example.samplestickerapp.StickerContentProvider.PUBLISHER_EMAIL;
-import static com.example.samplestickerapp.StickerContentProvider.PUBLISHER_WEBSITE;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_FILE_EMOJI_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_FILE_NAME_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_ICON_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_IDENTIFIER_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_NAME_IN_QUERY;
-import static com.example.samplestickerapp.StickerContentProvider.STICKER_PACK_PUBLISHER_IN_QUERY;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.ANDROID_APP_DOWNLOAD_LINK_IN_QUERY;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.IOS_APP_DOWNLOAD_LINK_IN_QUERY;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.LICENSE_AGREENMENT_WEBSITE;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.PRIVACY_POLICY_WEBSITE;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.PUBLISHER_EMAIL;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.PUBLISHER_WEBSITE;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.STICKER_FILE_EMOJI_IN_QUERY;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.STICKER_FILE_NAME_IN_QUERY;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.STICKER_PACK_ICON_IN_QUERY;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.STICKER_PACK_IDENTIFIER_IN_QUERY;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.STICKER_PACK_NAME_IN_QUERY;
+import static com.fracasitos.samplestickerapp.StickerContentProvider.STICKER_PACK_PUBLISHER_IN_QUERY;
 
 public class StickerPackLoader {
 
